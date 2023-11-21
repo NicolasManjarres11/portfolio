10ce2c2ad6e5e0750c3caaa03d86ceb08eb30d3f
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-11-20 20:26:57 -0500

    edición de home 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home flex'>
+      <div className='description grid items-center justify-center p-40'>
+        <h1 className=''>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </h1>
+        <button className=''>
+          Ver más
+        </button>
+      </div>
+      <div>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rtve.es%2Ftelevision%2F20221016%2Fbatman-actores-cine-saber-ganar%2F2333755.shtml&psig=AOvVaw1O7sE3DFJ9zTtPn8MDWYHl&ust=1700616189914000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCQgZH304IDFQAAAAAdAAAAABAE" alt='batman'/>
+      </div>
+    </div>
   )
 }
 
