ced2fb3de5c21d0cd1bb9e2116f1e40af2e2a6bb
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-11-23 22:36:51 -0500

    se ajusta por columnas la sección de proyectos, queda pendiente agregar los logos de las herramientas que se utilizaron en el proyecto

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -5,28 +5,32 @@ import { faCode, faLink } from '@fortawesome/free-solid-svg-icons'
 
 const Proyectos = () => {
   return (
-    <div id="proyectos" className='bg-[#302129] py-6 lg:py-12 lg:px-[150px] px-10'>
-      <div className='container mx-auto w-[600px]'>
+    <div id="proyectos" className='flex flex-col items-center gap-10 bg-[#302129] py-6 lg:py-12 lg:px-[150px] px-10'>
+      <div className='grid gap-10 items-center w-[600px]'>
           <div className='items-center justify-center'>
               <h1 className='text-center text-white p-6 font-abril text-4xl'>Proyectos</h1>
               <div>
                 {projects.map((project, index) => {
                   return (
-                    <div className='flex justify-center p-6 ' key={index}>
-                      <div>
-                          <img src={project.image} alt='' className='h-[150px] w-[150px] justify-start px-4 mr-4 left-8'/>
+                    <div className='grid gap-4 md:grid-cols-2 group mb-10' key={index}>
+                      <div className=''>
+                          <img src={project.image} alt='' className='h-[200px] w-auto justify-center px-4 mr-4 left-8'/>
                       </div>
                       <div>
                         <h2 className='font-ubunt font-bold text-2xl text-[#6249c7] mb-1 '>{project.tittle}</h2>
                         <p className='text-white font-barlow my-5'>{project.description}</p>
                         <div className='flex'>
-                        <a href={project.code} className='flex bg-[#6249c7] text-white font-ubuntu p-1.5 rounded-lg border-black hover:text-black hover:bg-white transition duration-300 mr-2'>
+                        <a href={project.code} className='flex bg-[#6249c7] text-white font-ubuntu p-1.5 rounded-lg border-black hover:text-black hover:bg-white transition duration-300 mr-2'
+                        target="_blank"
+                        rel="noopener noreferrer">
                           Code
                           <span className='ml-1' >
                             <FontAwesomeIcon icon={faCode} />
                           </span>
                         </a >
-                        <a href={project.live} className='flex bg-[#6249c7] text-white font-ubuntu p-1.5 rounded-lg border-black hover:text-black hover:bg-white transition duration-300'>
+                        <a href={project.live} className='flex bg-[#6249c7] text-white font-ubuntu p-1.5 rounded-lg border-black hover:text-black hover:bg-white transition duration-300'
+                        target="_blank"
+                        rel="noopener noreferrer">
                           Live
                           <span className='ml-1' >
                             <FontAwesomeIcon icon={faLink} />
