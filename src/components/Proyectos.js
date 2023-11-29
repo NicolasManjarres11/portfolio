34e1709bb3dd651f51515b35fd1c49239fcf4805
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-11-28 22:18:31 -0500

    se estiliza la parte de las imagenes de los proyectos, sigue la estilización del titulo, descripcion y tecnologías que se utilizaron

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {projects} from './Data'
-import bgImg from '../assets/img/bg.jpg'
+import bgImg from '../assets/img/bg.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode, faLink } from '@fortawesome/free-solid-svg-icons'
 
@@ -13,11 +13,13 @@ const Proyectos = () => {
             <div className='grid gap-9  lg:grid-cols-2' key={index}>
                 <a href={project.live} target='_blank' rel='noreferrer'>
                   <div className='rounded-xl overflow-hidden flex'>
-                    <span className='box-border border-bl'>
-                      <span>
-                          <img/>
+                    <span className='box-border inline-block overflow-hidden w-initial h-initial bg-none opacity-100 border-0 m-0 p-0 relative max-w-full'>
+                      <span className='box-border w-initial h-initial bg-none opacity-100 border-0 m- p-0 max-w-full'>
+                          <img className='block max-w-full w-initial h-initial bg-none opacity-100 border-0 m-0 p-0' alt='' aria-hidden="true" src={bgImg}/>
                       </span>
-                      <img src={project.image} className='rounded-lg duration-500 transition-all  hover:translate-x-7 hover:translate-y-7'  alt=''/>
+                      <img src={project.image} className='rounded-lg duration-500 transition-all translate-x-9 translate-y-9 hover:translate-x-4 hover:translate-y-4 absolute top-0 left-0 bottom-0 right-0 box-border p-0 border-none m-auto'  alt=''
+                      decoding='async'
+                      data-nimg='intrinsic'/>
                     </span>
                     
                   </div>
