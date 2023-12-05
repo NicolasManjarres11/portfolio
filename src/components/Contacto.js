7e8ad2c97273620f60eac5cd48455f9e234de130
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2023-12-04 21:52:08 -0500

    En proceso de edicion de componente contactame

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contacto = () => {
   return (
-    <div className='bg-black' style={{height: "500px"}}>
-      Contacto
+    <div className='bg-black py-16 px-16 flex  items-center justify-between'>
+      <div className='flex justify-center items-center lg:mx-[100px]'>
+        <h1 className='flex text-white font-abril font-bold text-5xl ml-10 mb-10 text-center justify-center'>
+          Contáctame
+        </h1>
+      </div>
     </div>
   )
 }
