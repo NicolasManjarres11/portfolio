d383fa279c2da0d8f0986ce33398cecc7eacb80b
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-02-19 21:13:00 -0500

    se hace prueba de carpeta build y despliega correctamente, se hará prueba desplegando a un hosting online

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import About from './components/About';
 import Contacto from './components/Contacto';
 import Home from './components/Home';
-import Navbar from './components/Navbar';
+
 import Perfil from './components/Perfil';
 import Proyectos from './components/Proyectos';
 import Skills from './components/Skills';
 import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
 
