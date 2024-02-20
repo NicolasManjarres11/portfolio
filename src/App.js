dd7c7f13a3aca3e30e16e03348dae42520ff2996
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-02-19 20:06:30 -0500

    se agrega clase App en  App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
 
   return (
-    <>
+    <div className='App'>
       <Navbar/>
       <Home />
       <About/>
@@ -18,7 +18,7 @@ function App() {
       <Skills/>
       <Proyectos/>
       <Contacto/>
-    </> 
+    </div> 
   );
 }
 
